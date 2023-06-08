b35ab4f6942e3208c0727e3218af2f3a315d3523
moreshk <moreshk@gmail.com>
2023-06-08 23:02:45 +1000

    upload all files

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -2,12 +2,18 @@
  * Change the namespace to the namespace on Pinecone you'd like to store your embeddings.
  */
 
+import Router from "next/router";
+import { supabase } from '@/lib/supabase';
+import { create } from "zustand";
+
 if (!process.env.PINECONE_INDEX_NAME) {
   throw new Error('Missing Pinecone index name in .env file');
 }
 
 const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
 
-const PINECONE_NAME_SPACE = 'cnn'; //namespace is optional for your vectors
+const PINECONE_NAME_SPACE = Router.query.chatbotId;
+
+// const PINECONE_NAME_SPACE = 'cnn'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
